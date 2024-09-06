427d78d602538d531ff0f79b56a015e5bc34fe07
Liam <liam.perl@ucas-edu.net>
2024-09-05 21:31:56 -0600

    easter egg added and changing vars

--- number game/main.py
+++ number game/main.py
@@ -1,17 +1,34 @@
-import random 
-loops = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
-num = random.randint(1, 100)
-print ("Welcome to GUESS THE NUMBER. You have 20 attempts to get the number then you lose. Good luck!")
-guess = int (input("guess from 1-100: "))
-for x in loops:
-	
-	if guess == num: 
-		print ("you got it! Game Over")
-		break
-	if guess > num:
-		guess = int (input("Its smaller: "))
-	if guess < num:
-		guess = int (input("its bigger: "))
-	if x ==20:
-		print ("game over")
-		break
+import random
+
+easterEggCount = 2
+maxGuessCount = 20
+
+minGuess = 1
+maxGuess = 100
+
+
+playCount = 0
+playAgain = True
+while playAgain:
+	playCount += 1
+
+	if playCount == easterEggCount:
+		print("WHY DID YOU WASTE YOUR TIME ON THIS DUMB GAME! DO SOMETHING BETIER WITH YOUR TIME!")
+		exit()
+
+
+	num = random.randint (minGuess,maxGuess)
+	print(f'Welcome the GUESS THE NUMBER! you have {maxGuessCount} attempts before you lose the game. good luck.')
+	guess = int(input(f'Guess a number {minGuess}-{maxGuess}: '))  
+
+	for x in range(maxGuessCount): 
+		if guess < num :
+			guess = int(input ("the number is larger: "))
+		if guess > num :
+			guess = int(input ("the number is smaller: "))
+		if guess == num : 
+			print ("you got it")
+			playAgain = input ("play again? (y/n): ")
+			if playAgain != "y":
+				playAgain = False
+			break
