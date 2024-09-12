09954dd7e053ce7ae8069303d691e28d3f0269c4
Liam <liam.perl@ucas-edu.net>
2024-09-11 20:49:55 -0600

    updated calculater

--- first project/calculater.py
+++ first project/calculater.py
@@ -1,12 +1,87 @@
-#liam celsius to fahrenheit calculater/profishency test
-yes = input("woud you like celsius to fahrenheit (y/n): ")
-if yes == "y":
-    tempofcelsius = int(input("what is the temp in celsius: ")) 
-    print ("the temp for fahrenheit is {tempofcelsius*(9/5)+32}")
-if yes == "n":
-    no = input("woud you like celsius to fahrenheit (y/n): ")
-    if no == "y":
-        tempforfahrenheit = int(input("what is the temp in fahrenheit: ")) 
-        print("the temp for celsius is {tempforfahrenheit-32*(5/9)}")
+operation = 0
+conferm = 0
+a = "n/a"
+b = "n/a"
+print("Hi this is Calcu. What do you want me to calculate today")
+def calcu():
+    operation = input("Do you want to do division, multiplication, subtraction, addition, modulo, or factoring: ")
+    if operation == "division" :
+        a = int(input("what is the first number:"))
+        b = int(input("what is the second number:"))
+        if b == 0 :
+            print("division by 0 error")
+            calcu()
+        else:
+            print(a,"/",b,"=",a/b)
+            calcu()
+    if operation == "multiplication" :
+        a = int(input("what is the first number:"))
+        b = int(input("what is the second number:"))
+        print(a,"X",b,"=",a*b)
+        calcu()
+    if operation == "subtraction" :
+        a = int(input("what is the first number:"))
+        b = int(input("what is the second number:"))
+        print(a,"-",b,"=",a-b)
+        calcu()
+    if operation == "addition" :
+        a = int(input("what is the first number:"))
+        b = int(input("what is the second number:"))
+        print(a,"+",b,"=",a+b)
+        calcu()
+    if operation == "modulo" :
+        a = int(input("what is the first number:"))
+        b = int(input("what is the second number:"))
+        print(a,"%",b,"=",a%b)
+        calcu()
+    if operation == "factoring" :
+        a = int(input("what is the first number:"))
+        b = int(input("what is the second number:"))
+        print(a,"^",b,"=",a**b)
+        calcu()
+    if operation == "stop" :
+        quit
+    print("Sorry I didn't understand")
+    calcu()
 
-quit
+
+
+
+operation = input("Do you want to do division, multiplication, subtraction, addition, modulo, or factoring:  ")
+if operation == "division" :
+    a = int(input("what is the first number:"))
+    b = int(input("what is the second number:"))
+else:
+    if b == 0 :
+        print("division by 0 error")
+        calcu()
+        print(a,"/",b,"=",a/b)
+        calcu()
+if operation == "multiplication" :
+    a = int(input("what is the first number:"))
+    b = int(input("what is the second number:"))
+    print(a,"X",b,"=",a*b)
+    calcu()
+if operation == "subtraction" :
+    a = int(input("what is the first number:"))
+    b = int(input("what is the second number:"))
+    print(a,"-",b,"=",a-b)
+    calcu()
+if operation == "addition" :
+    a = int(input("what is the first number:"))
+    b = int(input("what is the second number:"))
+    print(a,"+",b,"=",a+b)
+    calcu()
+if operation == "modulo" :
+    a = int(input("what is the first number:"))
+    b = int(input("what is the second number:"))
+    print(a,"%",b,"=",a%b)
+    calcu()
+if operation == "factoring" :
+    a = int(input("what is the first number:"))
+    b = int(input("what is the second number:"))
+    print(a,"^",b,"=",a**b)
+    calcu()
+print("Sorry I didn't understand")
+
+calcu()
